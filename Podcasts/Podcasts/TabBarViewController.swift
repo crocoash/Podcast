01cfaf7460368ffd8743ce55c12446683d8e8408
IvanEvseenko <ivan.evseenko.2017@gmail.com>
2021-10-26 18:45:04 +0300

    commit player created

--- Podcasts/Podcasts/TabBarViewController.swift
+++ Podcasts/Podcasts/TabBarViewController.swift
@@ -8,34 +8,22 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
-    override func viewWillAppear(_ animated: Bool) {
-        //createPlayer()
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         tabBarController?.viewControllers = []
+        tabBar.bounds.height
         
+        showPlayer()
     }
-    func createPlayer() {
+    
+}
 
-        let viewOverTabBar = UIView(frame: CGRect(x: 0, y: self.tabBar.frame.origin.y-120, width: self.tabBar.frame.size.width, height: 20))
-        //let viewOverTabBar = UIView(frame: CGRect(x: 0, y: self.tabBar.bounds.origin.y-10, width: self.tabBar.frame.size.width, height: 80))
-            viewOverTabBar.backgroundColor = UIColor.systemPink
-        
-        
-
-            //viewOverTabBar.layer.cornerRadius = viewOverTabBar.frame.size.height/2
-            viewOverTabBar.layer.masksToBounds = false
-            viewOverTabBar.layer.shadowColor = UIColor.black.withAlphaComponent(0.5).cgColor
-            viewOverTabBar.layer.shadowRadius = 5.0
-            viewOverTabBar.layer.shadowOffset = CGSize(width: 0.0, height: -5.0)
-            viewOverTabBar.layer.shadowOpacity = 0.5
-
-            //tabBar.addSubview(viewOverTabBar)
-            view.addSubview(viewOverTabBar)
-        }
+extension TabBarViewController {
+    private func showPlayer() {
+        let playerView = UINib(nibName: "SmallPlayerView", bundle: .main).instantiate(withOwner: nil, options: nil).first as! SmallPlayerView
+        playerView.configurPlayer()
+        view.addSubview(playerView)
+    }
 }
