95e8ca08b86c0f4279e75757e61552420aa0dc84
IvanEvseenko <ivan.evseenko.2017@gmail.com>
2021-10-26 13:54:45 +0300

    commit

---
+++ Podcasts/Podcasts/SmallPlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  SmallPlayerView.swift
+//  Podcasts
+//
+//  Created by mac on 26.10.2021.
+//
+
+import UIKit
+
+class SmallPlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
