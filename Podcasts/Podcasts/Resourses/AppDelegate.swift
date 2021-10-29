75af32e5ce5ccc4aaa22e95e06079493dd4460b1
Anton Tsvetkov <crocoash@gmail.com>
2021-10-29 15:47:39 +0300

    conflicts

--- Podcasts/Podcasts/Resourses/AppDelegate.swift
+++ Podcasts/Podcasts/Resourses/AppDelegate.swift
@@ -10,11 +10,7 @@ import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-<<<<<<< HEAD:Podcasts/Podcasts/Resourses/AppDelegate.swift
-
-=======
     
->>>>>>> Features/Registration:Podcasts/Podcasts/AppDelegate.swift
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         return true
@@ -24,13 +20,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-<<<<<<< HEAD:Podcasts/Podcasts/Resourses/AppDelegate.swift
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-=======
->>>>>>> Features/Registration:Podcasts/Podcasts/AppDelegate.swift
 }
