006bc81f64ca466b445f5259dc9d6f0d3edfc40a
Anton Tsvetkov <crocoash@gmail.com>
2021-11-01 12:31:25 +0200

    fixed conflicts

--- Podcasts/Podcasts/View/ViewControllers/TabBarViewController.swift
+++ Podcasts/Podcasts/View/ViewControllers/TabBarViewController.swift
@@ -6,17 +6,30 @@ class TabBarViewController: UITabBarController {
     
     private var newPlayerVC = PlayerViewController()
     
-    
     lazy var constraintsSmallPlayer: [NSLayoutConstraint] = [
         newPlayerVC.view.heightAnchor.constraint(equalTo: tabBar.heightAnchor),
         newPlayerVC.view.widthAnchor.constraint(equalTo: view.widthAnchor),
         newPlayerVC.view.bottomAnchor.constraint(equalTo: tabBar.topAnchor, constant: -5)
     ]
+    
     lazy var constraintsBigPlayer: [NSLayoutConstraint] = [
         newPlayerVC.view.topAnchor.constraint(equalTo: view.topAnchor),
         newPlayerVC.view.widthAnchor.constraint(equalTo: view.widthAnchor),
         newPlayerVC.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
     ]
+    
+    lazy var searchVC: SearchViewController = {
+        let searchVC = storyboard?.instantiateViewController(withIdentifier: SearchViewController.identifier) as! SearchViewController
+        searchVC.tabBarItem.title = "Search"
+        searchVC.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        return searchVC
+    }()
+    
+    lazy var playListVc: PlaylistTableViewController = {
+        let playListVc =  storyboard?.instantiateViewController(withIdentifier: PlaylistTableViewController.identifier) as! PlaylistTableViewController
+        playListVc.navigationItem.title = "Playlist"
+        return playListVc
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,44 +38,20 @@ class TabBarViewController: UITabBarController {
     }
     
     private func configureTabBar() {
-        let main = UIStoryboard(name: "Main", bundle: nil)
-        let searchVC = main.instantiateViewController(identifier: "SearchViewControllerID") as SearchViewController
-        let searchItem = UITabBarItem(title: "Search", image: nil, selectedImage: nil)
-        searchVC.tabBarItem = searchItem
-        let playListVC = main.instantiateViewController(identifier: "PlaylistTableViewControllerID") as PlaylistTableViewController
-        let playListItem = UITabBarItem(title: "PlayList", image: nil, selectedImage: nil)
-        
-        playListVC.tabBarItem = playListItem
-        self.viewControllers = [searchVC,playListVC]
-    }
-    
-    private func addPlayer() {
-        self.addChild(newPlayerVC)
-        
-
-        view.addSubview(newPlayerVC.view)
-        newPlayerVC.didMove(toParent: self)
-        newPlayerVC.view.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate(constraintsSmallPlayer)
-        createAndAddGestures(to: newPlayerVC)
-
-        let searchVC = storyboard?.instantiateViewController(withIdentifier: SearchViewController.identifier) as! SearchViewController
-        
-        searchVC.tabBarItem.title = "Search"
-        searchVC.tabBarItem.image = UIImage(systemName: "magnifyingglass")
-
-        let playListVc =  storyboard?.instantiateViewController(withIdentifier: PlaylistTableViewController.identifier) as! PlaylistTableViewController
-
-        playListVc.navigationItem.title = "Playlist"
-        
         let navigationVC = UINavigationController(rootViewController: playListVc)
         navigationVC.tabBarItem.title = "Playlist"
         navigationVC.tabBarItem.image = UIImage(systemName: "book")
         
         viewControllers = [searchVC, navigationVC]
-        
-//        showPlayer()
-
+    }
+    
+    private func addPlayer() {
+        self.addChild(newPlayerVC)
+        view.addSubview(newPlayerVC.view)
+        newPlayerVC.didMove(toParent: self)
+        newPlayerVC.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(constraintsSmallPlayer)
+        createAndAddGestures(to: newPlayerVC)
     }
     
     private func updatePlayerConstraints() {
@@ -85,15 +74,15 @@ class TabBarViewController: UITabBarController {
     }
     
     @objc func respondToSwipe(gesture: UISwipeGestureRecognizer) {
-            switch gesture.direction {
-            case .up:
-                updatePlayerConstraints()
-                newPlayerVC.isPlayerBig = true
-            case .down:
-                updatePlayerConstraints()
-                newPlayerVC.isPlayerBig = false
-            default:
-                break
-            }
+        switch gesture.direction {
+        case .up:
+            updatePlayerConstraints()
+            newPlayerVC.isPlayerBig = true
+        case .down:
+            updatePlayerConstraints()
+            newPlayerVC.isPlayerBig = false
+        default:
+            break
+        }
     }
 }
