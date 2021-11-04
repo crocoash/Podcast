3a3c684da37e74416c498dd850502c7bcaf36351
Andrii Hulevych <andriihulevych@gmail.com>
2021-11-04 14:40:30 +0200

    added detailview for podcasts

--- Podcasts/Podcasts/View/ViewControllers/DetailViewController.swift
+++ Podcasts/Podcasts/View/ViewControllers/DetailViewController.swift
@@ -13,12 +13,40 @@ protocol DetailViewControllerDelegate: AnyObject {
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet private weak var episodeImage: UIImageView!
+    @IBOutlet private weak var episodeName: UILabel!
+    @IBOutlet private weak var collectionName: UILabel!
+    @IBOutlet private weak var descriptionTextView: UITextView!
+    
+    private var index : Int!
+    private var image : UIImage!
+    private var episode : String!
+    private var collection : String!
+    private var episodeDescription : String!
+    
     weak var delegate: DetailViewControllerDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupView()
     }
-
+    @IBAction private func listenButtonOnTouchUpInside(_ sender: UIButton) {
+        //delegate?.detailViewController(sender: self, playButtonDidTouchFor: <#T##Podcast#>)
+    }
+    
+    func receivePodcastInfoAndIndex(index: Int, image: UIImageView, episode: String, collection: String, episodeDescription: String) {
+        self.index = index
+        self.image = image.image
+        self.episode = episode
+        self.collection = collection
+        self.episodeDescription = episodeDescription
+    }
+    
+    private func setupView(){
+        episodeImage.image = image
+        episodeName.text = episode
+        collectionName.text = collection
+        descriptionTextView.text = episodeDescription
+    }
+    
 }
