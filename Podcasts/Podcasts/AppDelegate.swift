47a6864627651c0922b096a7a8dd03d3f8d81277
Andrii Hulevych <andriihulevych@gmail.com>
2021-11-11 17:51:22 +0200

    redesigned buttons, added download on save button in detail

--- Podcasts/Podcasts/AppDelegate.swift
+++ Podcasts/Podcasts/AppDelegate.swift
@@ -13,6 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var backgroundSessionCompletionHandler: (() -> Void)?
     
+    let orientationLock = UIInterfaceOrientationMask.portrait
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return self.orientationLock
+        
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         return true
