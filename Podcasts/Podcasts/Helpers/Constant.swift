056a01d40fc990a098efa62766f48a0df6ff1bb3
Tsvetkov Anton <crocoash@gmail.com>
2022-04-05 15:52:27 +0300

    merge

--- Podcasts/Podcasts/Helpers/Constant.swift
+++ Podcasts/Podcasts/Helpers/Constant.swift
@@ -16,7 +16,8 @@ enum UrlRequest1 {
         case .podcast(let string):
             return "https://itunes.apple.com/search?term=\(string)&entity=podcastEpisode"
         case .authors(let string):
-            return "https://itunes.apple.com/search?term=\(string)&media=podcast&entity=podcastAuthor"
+
+            return "\(adress) + \(string) +"&media=podcast&entity=podcastAuthor"
         }
     }
 }
